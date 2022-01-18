7b6b0bd9d3e4cf6efb3631f6320871cc71acab03
Pedro <pedro.trabalhos@outlook.com>
2022-01-18 18:17:43 -0300

    update and add body-parser

--- src/routes/stock.js
+++ src/routes/stock.js
@@ -5,5 +5,9 @@ router.get('/mov', (req, res)=>{
      res.render('stock_mov_page');
 });
 
+router.post('/new', function (req, res) {
+     console.log(req.body.nameStock)
+})
+
 
 module.exports = router;
